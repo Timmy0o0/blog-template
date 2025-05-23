c937e4b499b7f619d3d5067898490d96adec2b78
timmy <yangtmeco@outlook.com>
2025-05-23 12:38:01 +0800

    feat: add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown()],
+  integrations: [react(), partytown(), mdx()],
 });
