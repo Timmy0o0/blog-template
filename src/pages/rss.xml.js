441a45bc0f2f000c4fcef267ed6cc8582bfa8df4
timmy <yangtmeco@outlook.com>
2025-06-02 01:05:11 +0800

    feat: add rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,21 @@
+import rss from "@astrojs/rss";
+import { SITE_DESCRIPTION, SITE_TITLE } from "@config";
+import { getCollection } from "astro:content";
+
+export async function GET(context) {
+  const posts = await getCollection("blog");
+
+  const items = posts.map((post) => ({
+    title: post.data.title,
+    pubDate: post.data.pubDate,
+    description: post.data.description,
+    link: `/${post.id}`,
+  }));
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: items,
+  });
+}
